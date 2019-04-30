c07a5ab8e9d66666fa590764f88435da420eb6e1
torin <torin@live.unc.edu>
2019-04-30 10:19:39 -0400

    add initial site files

---
+++ precache-manifest.e08f1dffe582ec208097fa6ce9047b7f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "421597f7b1bea8cd6e5e",
+    "url": "/basic-income/static/js/runtime~main.f4dc56a9.js"
+  },
+  {
+    "revision": "502e0fcd9b7a21ac5bd8",
+    "url": "/basic-income/static/js/main.117ef993.chunk.js"
+  },
+  {
+    "revision": "35d78a527cfd43cb2fde",
+    "url": "/basic-income/static/js/2.fd2fd18f.chunk.js"
+  },
+  {
+    "revision": "502e0fcd9b7a21ac5bd8",
+    "url": "/basic-income/static/css/main.8178431a.chunk.css"
+  },
+  {
+    "revision": "7657fea539a8d0f20d7ef0f6808f39d5",
+    "url": "/basic-income/index.html"
+  }
+];
